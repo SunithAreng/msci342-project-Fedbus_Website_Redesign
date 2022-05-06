98c652c09017b0572b759f3e02cff0041431810f
Rama <r2alaghb@uwaterloo.ca>
2022-05-05 20:00:41 -0400

    first commit

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -135,7 +135,7 @@ class Home extends Component {
           >
             {this.state.mode === 0 ? (
               <React.Fragment>
-                Welcome to MSci245!
+                Hello World!
               </React.Fragment>
             ) : (
               <React.Fragment>
