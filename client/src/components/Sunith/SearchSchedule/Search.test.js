f94d2211ff498f2b91dbe33291764549c6b11a33
sareng <sareng@uwaterloo.ca>
2023-02-24 16:36:02 -0500

    some work

--- client/src/components/Sunith/SearchSchedule/Search.test.js
+++ client/src/components/Sunith/SearchSchedule/Search.test.js
@@ -2,6 +2,32 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Search from './Search';
 
-it('changes the class when hovered', () => {
+describe('RestaurantList', () => {
+    // const restaurants = [
+    //   {id: 1, name: 'Sushi Place'},
+    //   {id: 2, name: 'Pizza Place'},
+    // ];
+    // let loadRestaurants;
 
+    // function renderComponent() {
+    //   loadRestaurants = jest.fn().mockName('loadRestaurants');
+
+    //   render(
+    //     <RestaurantList
+    //       loadRestaurants={loadRestaurants}
+    //       restaurants={restaurants}
+    //     />,
+    //   );
+    // }
+    // it('loads restaurants on first render', () => {
+    //   renderComponent();
+
+    //   expect(loadRestaurants).toHaveBeenCalled();
+    // });
+    // it('displays the restaurants', () => {
+    //   renderComponent();
+
+    //   expect(screen.getByText('Sushi Place')).toBeInTheDocument();
+    //   expect(screen.getByText('Pizza Place')).toBeInTheDocument();
+    // });
 });
