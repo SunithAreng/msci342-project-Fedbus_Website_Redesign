38261b1db2d093b6750716f6d4f1f66ee179384f
sareng <sareng@uwaterloo.ca>
2023-02-25 15:25:30 -0500

    packages added

--- client/src/components/Sunith/SearchSchedule/Search.test.js
+++ client/src/components/Sunith/SearchSchedule/Search.test.js
@@ -3,10 +3,8 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import renderer from 'react-test-renderer';
 import SearchSchdeule from './Search';
 import TimePreference from './Search';
-import Search from './Search';
 
 test("blah", () => {
     render(<SearchSchdeule />);
-
-    // expect(getOrigin()).toHaveBeenCalled();
+    expect(SearchSchedule.getOrigin()).toHaveBeenCalled();
 })
