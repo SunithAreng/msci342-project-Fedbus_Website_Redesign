bca74aa1ffac9cde3a2acd497a625a85e3d54cda
r28yao <71105775+r28yao@users.noreply.github.com>
2023-03-03 05:26:22 -0500

    Add files via upload
    location

---
+++ client/src/components/Harry/Location/location/App.js
@@ -0,0 +1,107 @@
+import './style.css'
+import logo from './logo.png'
+
+function App() {
+  return (
+    <>
+    <br/>
+    <img src={logo} alt=""/>
+    <br/><br/>
+<hr style= {{backgroundColor: 'black',height:'4px', border: '0px'}}/>
+
+    <title>FedBus Bus Stops</title>
+    <h1>Bus Stops</h1>
+
+  <h3>McCowan (Scarborough)</h3>
+  <h5>The McCowan Subway stop at the corner of Bushby and McCowan.</h5>
+
+  
+    <iframe style= {{width:"80%", height:"40%", borderRadius: '14px'}}src="https://maps.google.com/maps?q=43.774835,-79.251452&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    <br /><br />
+
+    <h3>York Mills - New Stop</h3>
+  <h5>Old York Mills Road - near the Shell gas station and York Mills Station Kiss n' Ride.</h5>
+  <iframe style= {{width:"80%", height:"40%", borderRadius: '14px'}} src="https://maps.google.com/maps?q=43.743558,%20-79.405683&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" ></iframe>
+    <br /><br />
+
+    <h3>Richmond Hill</h3>
+  <h5>The York Region Transit and Viva terminal on 8675 Yonge Street. It was built near the ramp that connects Highway 7
+    and Yonge Street.</h5>
+  <div id="richmondhill"></div>
+  <iframe style= {{width:"80%", height:"40%", borderRadius: '14px'}} src="https://maps.google.com/maps?q=43.839636,%20%20-79.425580&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+
+
+  <br /><br />
+
+  
+
+  <h3>London</h3>
+  <h5>At the bus terminal in CF Masonville Place at the corner of Richmond and Fanshawe.</h5>
+  <div id="london"></div>
+
+  <iframe style= {{width:"80%", height:"40%", borderRadius: '14px'}} src="https://maps.google.com/maps?q=43.025950,%20-81.281260&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+<br /><br />
+
+  <h3>Hamilton</h3>
+  <h5>Sheraton Hotel entrance at Jackson Square.</h5>
+  <div id="hamilton"></div>
+
+  <iframe style= {{width:"80%", height:"40%", borderRadius: '14px'}} src="https://maps.google.com/maps?q=43.257651,%20%20-79.872418&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+
+  <br /><br />
+  
+
+  <h3>Burlington</h3>
+  <h5>Aldershot GO Station, South Parking Lot on Masonry Court.</h5>
+  <div id="burlington"></div>
+
+  <iframe style= {{width:"80%", height:"40%", borderRadius: '14px'}} src="https://maps.google.com/maps?q=43.312100,%20-79.851843&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+
+  <br /><br />
+
+  <h3>Markham</h3>
+  <h5>CF Markville - outlying mall parking off of Bullock Drive - near Best Buy / Pickel Barrel but in outlying parking lots.</h5>
+
+  <iframe style= {{width:"80%", height:"40%", borderRadius: '14px'}} src="https://maps.google.com/maps?q=43.868462,%20-79.291938&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+
+    <br /><br /><br />
+    <hr style={{ height: '2px', backgroundColor: 'black', border:'0'}}/>
+    <h3>Useful Information</h3>
+  <h5>1. The Fed Bus is a school bus. Only baggage that will fit on your lap or under your seat will be accommodated.
+    <br/>
+    2. Tickets are limited, please purchase early!
+    <br/>
+    3. You must show your Watcard for every ticket purchase and upon boarding.
+    <br/>
+    4. The Fed Bus does not run during examination periods. When classes finish, so does the Fed Bus.
+    <br/>
+    5. No refunds on ticket purchases. Please arrive 15 minutes prior to your departure time.
+  </h5>
+
+  
+  <hr style={{ height: '2px', backgroundColor: 'black',border:'0'}}/>
+  <h3>Contact</h3>
+  <h5>Student Life Centre - Turnkey Desk:
+    <br />
+    200 University Ave West
+    <br/ >
+    Waterloo, ON N2L 3G1
+    <br/>
+    519-888-4434
+    <br/ >
+    turnkeys@uwaterloo.ca <br/>
+  </h5>
+  
+  <h5>Fed Bus Email:
+  <br />
+    fedbus@wusa.ca
+  </h5>
+
+  <br/>
+    
+    </>
+
+  );
+}
+
+export default App;
