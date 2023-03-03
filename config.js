662fab0f8ba9713cc72275b59328ca79d8c941ab
r28yao <r28yao@uwaterloo.ca>
2023-03-03 11:47:25 -0500

    config removed

--- config.js
+++
@@ -1,8 +0,0 @@
-let config = {
-  host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
-  user    : 'r28yao',
-  password: 'wz~9LY@8q.dmx>bW',
-  database: 'sareng'
-};
- 
-module.exports = config;
