e71859854e3f00ffbf298f9fd2ce96d5f636b030
SunithAreng <90345363+SunithAreng@users.noreply.github.com>
2023-03-03 01:15:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
   host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
-  user    : 'sareng',
-  password: 'TbmP1234',
-  database: 'sareng'
+  user    : '',
+  password: '',
+  database: ''
 };
  
 module.exports = config;
