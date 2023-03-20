0676e646c36da321c115554aa9ae5112ddd788a2
sareng <sareng@uwaterloo.ca>
2023-03-20 00:33:30 -0400

    UI modification

--- client/src/components/Sunith/SearchSchedule/DateSelection.js
+++ client/src/components/Sunith/SearchSchedule/DateSelection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 
-export const DateSelection = ({ onChange, date }) => {
+export const DateSelection = ({ onChange, date, label }) => {
 
     const disableUnusedDates = (date) => {
         const day = date.getDay();
@@ -18,7 +18,7 @@ export const DateSelection = ({ onChange, date }) => {
                     format="MM/dd/yyyy"
                     margin="normal"
                     id="date-picker-inline"
-                    label="What date?"
+                    label={label}
                     value={date}
                     onChange={onChange}
                     disablePast
